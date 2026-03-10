5a6eef7a9493c7381a04abb324330b65556b4de7
Ruben Alfonseca <ralfcab2302@g.educaand.es>
2026-03-10 12:20:42 +0100

    feat: agregar página de error y rutas correspondientes; implementar componente de error con pruebas

--- paqtrack_v2/frontend/frontend-angular/src/app/app.routes.ts
+++ paqtrack_v2/frontend/frontend-angular/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
-
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/login/login').then((m) => m.Login),
-  },{path:'login'},
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login').then((m) => m.Login),
+  },
   {
     path: 'dashboard',
     loadComponent: () => import('./pages/dashboard/dashboard').then((m) => m.Dashboard),
@@ -12,5 +15,9 @@ export const routes: Routes = [
   {
     path: 'busqueda',
     loadComponent: () => import('./pages/busqueda/busqueda').then((m) => m.Busqueda),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/error/error').then((m) => m.Error),
   },
 ];
